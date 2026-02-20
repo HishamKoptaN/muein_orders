a28109367d8d940ca228468d85a549bd15284d64
Hisham Mohamed <heshamkoptan@gmail.com>
2026-02-20 13:02:33 +0200

    sc

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -37,8 +37,8 @@ class AppRouter {
         return getIt<AuthBloc>().state.maybeWhen(
           authenticated: () {
             if (isGuestRoute || location == '/${AuthView.routeName}') {
-              return '/${OrderDocsView.routeName}';
-              // return '/${HomeView.routeName}';
+              return '/${HomeView.routeName}';
+              // return '/${OrderDocsView.routeName}';
             }
             return null;
           },
