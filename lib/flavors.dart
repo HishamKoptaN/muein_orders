4256d84ff91874120c87e621be236d2bab62e5dc
Hisham Mohamed <heshamkoptan@gmail.com>
2026-04-10 23:28:13 +0200

    sc

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,20 +1,20 @@
-enum Flavor {
-  dev,
-  prod,
-}
+// enum Flavor {
+//   dev,
+//   prod,
+// }
 
-class F {
-  static late final Flavor appFlavor;
+// class F {
+//   static late final Flavor appFlavor;
 
-  static String get name => appFlavor.name;
+//   static String get name => appFlavor.name;
 
-  static String get title {
-    switch (appFlavor) {
-      case Flavor.dev:
-        return 'Muein Orders Dev';
-      case Flavor.prod:
-        return 'Muein Orders';
-    }
-  }
+//   static String get title {
+//     switch (appFlavor) {
+//       case Flavor.dev:
+//         return 'Muein Orders Dev';
+//       case Flavor.prod:
+//         return 'Muein Orders';
+//     }
+//   }
 
-}
+// }
