b5f68b2bd59b442a361b76351874874e081cfd9e
Hisham Mohamed <heshamkoptan@gmail.com>
2026-04-10 23:05:57 +0200

    sc

--- lib/main_dev.dart
+++
@@ -1,7 +0,0 @@
-import 'flavors.dart';
-import 'main.dart' as main_app;
-
-void main() {
-  F.appFlavor = Flavor.dev;
-  main_app.main();
-}
--- lib/main_prod.dart
+++
@@ -1,7 +0,0 @@
-import 'flavors.dart';
-import 'main.dart' as main_app;
-
-void main() {
-  F.appFlavor = Flavor.prod;
-  main_app.main();
-}
