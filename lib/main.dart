a4eb8ee544ba2f037414668c2ddeb5610e08805e
Hisham Mohamed <heshamkoptan@gmail.com>
2026-04-10 16:03:51 +0200

    sc

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
-  F.appFlavor = Flavor.values.firstWhere(
-    (element) => element.name == appFlavor,
-  );
-
+void main() async {
+  F.appFlavor = Flavor.values.firstWhere((element) {
+    return element.name == appFlavor;
+  });
   runApp(const App());
 }
