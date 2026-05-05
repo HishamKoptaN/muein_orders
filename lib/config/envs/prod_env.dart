ae98d6e64ac16e4d9664d44d226efc0f8cf79fda
Hisham Mohamed <heshamkoptan@gmail.com>
2026-05-05 09:46:37 +0300

    sc

--- lib/config/envs/prod_env.dart
+++ lib/config/envs/prod_env.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import '../env.dart';
 import '../firebase/prod_firebase_options.dart';
 
-class ProdEnv implements Env {
+class ProdEnv extends Env {
   @override
   FirebaseOptions get firebaseOptions => ProdFirebaseOptions.currentPlatform;
   @override
